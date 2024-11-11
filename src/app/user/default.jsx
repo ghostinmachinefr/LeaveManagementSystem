2d51d4391fa282c07e1cc08cb0c7caef0ae2c777
Anushka Gupta <anushkayol001@gmail.com>
2024-11-11 09:41:00 +0530

    second commit

---
+++ src/app/user/default.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Default = () => {
+    return (
+        <div>
+            <h1>Welcome to the User Panel</h1>
+            <p>This is the default page for the user section.</p>
+        </div>
+    );
+};
+
+export default Default;
